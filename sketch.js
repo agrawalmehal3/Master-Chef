0bc84a0a7cf32fa87fdca462e514245a03a33349
agrawalmehal3 <agrawalmehal3>
2020-07-19 16:15:05 -0700

    added recipe

--- sketch.js
+++ sketch.js
@@ -3,23 +3,23 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1;
-var box2;
+var recipe1;
+var recipe2;
 var ground
 function setup(){
     var canvas = createCanvas(400,400);
     engine = Engine.create();
     world = engine.world;
 
-    box1 = new Box(200,300,50,50);
-    box2 = new Box(240,100,50,100);
+    recipe1 = new Box(200,300,50,50);
+    recipe2 = new Box(240,100,50,100);
     ground= new Ground(200,380,400,20);
 }
 
 function draw(){
     background(0);
     Engine.update(engine);
-    box1.display();
-    box2.display();
+    recipe1.display();
+    recipe2.display();
     ground.display();
 }
